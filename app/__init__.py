19f297aafc991bd5982c65e38bd95e2cfe0796f2
curti-s <matthewscurtis81@gmail.com>
2018-11-28 10:57:05 +0300

    [Start #162235868] Create view endpoints for redflags

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Resource, Api
 
 # local import
 from instance.config import app_config
+from .api.v1.views import redflag as rf
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     api = Api(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    app.register_blueprint(rf)
     return app
---
+++ app/api/v1/views/redflag_view.py
@@ -0,0 +1,76 @@
+from flask import request, jsonify, make_response, Blueprint, abort
+from flask_restful import Resource, Api, reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('message', type=str)
+
+redflag_list = [
+    {    
+        "status": 201,
+        "data": [{
+                "id": 0,
+                "message": "Created  red-flag record"
+        }]
+    },
+    {    
+        "status": 201,
+        "data": [{
+                "id": 1,
+                "message": "Created  red-flag record"
+        }]
+    }
+]
+
+class RedFlags(Resource):
+    """Redflags, lists all red flags and lets you create new ones"""
+    
+    def __init__(self):
+        self.db = redflag_list
+    
+    # create a red flag
+    def post(self):
+        response = {
+                    "status": 201,
+                    "data": [
+                        {
+                            "id": self.db[-1]['data'][0]['id']+1,
+                            "message": "Create red-flag record"
+                        }
+                    ]
+            }
+        self.db.append(response)
+        return make_response(jsonify(response),201) 
+
+    # get all red flags
+    def get(self):
+        response = self.db
+        return make_response(jsonify(response),200) 
+    
+
+class RedFlag(Resource):
+    """Redflag, shows a single red flag and lets you delete it"""
+    
+    def __init__(self):
+        self.db = redflag_list
+
+    # get a red flag
+    def get(self,id):
+        response = self.db[id]
+        if len(response) < 0:
+            abort(404)
+        return make_response(jsonify(response),200)
+
+    # edit a red flag comment / locaion
+    def put(self,id):
+        json_data = request.get_json()
+        args = parser.parse_args()
+        response = self.db[id]
+        response['data'][0]['message'] = args.message
+        return make_response(jsonify(response),201)        
+
+
+    # delete a red flag
+    def delete(self,id):
+        response = self.db
+        del response[id]
+        return make_response(jsonify(response),201)  
