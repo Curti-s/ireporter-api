51ed6aad0a67fd473376c2d43755ec8bb852aa93
curti-s <matthewscurtis81@gmail.com>
2018-12-01 21:17:39 +0300

    [Update #162341804] Update route

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ def create_app(config_name):
     app.register_blueprint(rf)
     app.register_blueprint(incident_api)
     api = Api(app)
-    api.add_resource(RedFlags,'/redflag/')
+    api.add_resource(RedFlags,'/','/redflag/')
     api.add_resource(RedFlag, '/redflag/<int:id>/')
     api.add_resource(IncidentApi,'/incident-api/')
     return app
