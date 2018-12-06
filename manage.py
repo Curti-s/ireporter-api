311b289600df745811fd80bafa6e9eb93102afe3
curti-s <matthewscurtis81@gmail.com>
2018-12-06 18:38:22 +0300

    [Update #162459188] Update database configurations with flask-script

--- manage.py
+++ manage.py
@@ -4,22 +4,38 @@ from datetime import datetime
 from flask import Flask
 from flask_script import Manager
 from app import create_app
+from instance.config_db import connection 
 
 app = create_app('development')
 manager = Manager(app)
 
-def connection():
-    """ Read connection params and connect to db"""
-    # read connection parameters
-    conn = psycopg2.connect(dbname="ireporter_api")
-    return conn
 
 @manager.command
-def init_db():
+def init_dev_db():
     with closing(connection()) as conn, conn.cursor() as cur:
         with app.open_instance_resource('createdb.sql', mode='r') as f:
             cur.execute(f.read())
         conn.commit()
+
+@manager.command
+def init_test_db():
+    with closing(connection()) as conn, conn.cursor() as cur:
+        with app.open_instance_resource('createdb.sql',mode='r') as f:
+            cur.execute(f.read())
+        conn.commit()
+
+@manager.command
+def drop_tables():
+    with closing(connection()) as conn, conn.cursor() as cur:
+        commands = (
+            """DROP TABLE IF EXISTS users CASCADE""",
+            """DROP TABLE IF EXISTS incidents CASCADE""",
+            """DROP TYPE IF EXISTS incident_type CASCADE""",
+            """DROP TYPE IF EXISTS current_status CASCADE""")
+        for command in commands:
+            cur.execute(command)
+        conn.commit()
+
 
 if __name__ == '__main__':
     manager.run()
